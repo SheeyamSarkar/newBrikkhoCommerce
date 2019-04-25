505fef0f8891ac530abc1c0f6ecb4c4fbd91c766
SheeyamSarkar <sheeyamcse@gmail.com>
2019-04-25 06:03:51 +0600

    dashboard with front view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
-@extends('layouts.app')
+@extends('backend.marchent.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('contentb')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div id="content-wrapper">
+
+
+        <!-- Page Content -->
+        <h1>Blank Page</h1>
+        <hr>
+        <p>This is a great starting point for new custom pages.</p>
+
     </div>
-</div>
+    <!-- /.container-fluid -->
 @endsection
+<!-- footer -->
